aa5cce06f8ae9dfeac24398a2e9c6446b6ea9dab
Yeonzel <hayeon9792@gmail.com>
2023-07-26 15:06:54 +0900

    Update Customer.java
    사용자 phoneNum 입력 함수 추가

--- src/Customer.java
+++ src/Customer.java
@@ -1,16 +1,41 @@
 public class Customer {
-	private String id; // Private attribute (can be accessed only within the class)
+	private int id; // Private attribute (can be accessed only within the class)
 	private Alarm alarm; // Private attribute (can be accessed only within the class)
 
 	// Constructor
-	public Customer(String id) {
+	public Customer(int id) {
 		this.id = id;
 		this.alarm = new Alarm();
 	}
 
 	// Getters and Setters
-	public String getId() {
+	public int getId() {
 		return id;
+	}
+
+	public String inputPhoneNumber() {
+        Random random = new Random();
+        StringBuilder phoneNumber = new StringBuilder();
+
+        for (int i = 0; i < 3; i++) {
+            int digit = random.nextInt(10); // 0부터 9까지의 랜덤한 숫자 생성
+            phoneNumber.append(digit);
+        }
+        phoneNumber.append("-");
+
+        for (int i = 0; i < 4; i++) {
+            int digit = random.nextInt(10);
+            phoneNumber.append(digit);
+        }
+        phoneNumber.append("-");
+
+        for (int i = 0; i < 4; i++) {
+            int digit = random.nextInt(10);
+            phoneNumber.append(digit);
+        }
+
+        return phoneNumber.toString();
+    	
 	}
 
 	// Private methods to manage createTime and lastOrderTime
